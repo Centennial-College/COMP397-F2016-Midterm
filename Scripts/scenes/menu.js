d056d8a1aa78670660a7925fe39b55e969f891d2
Kevin Ma <kbma.kevin@gmail.com>
2016-10-18 09:50:15 -0400

    added menu title label

--- Scripts/scenes/menu.js
+++ Scripts/scenes/menu.js
@@ -20,12 +20,17 @@ var scenes;
             // bg img
             this._backgroundImage = new createjs.Bitmap(assets.getResult("Bg"));
             this.addChild(this._backgroundImage);
+            // title Label
+            this._menuTitleLabel = new objects.Label("Cops and Robbers", "90px Comic Sans MS", "#f7e907", config.Screen.CENTER_X, config.Screen.CENTER_Y - 100);
+            this._menuTitleLabel.shadow = new createjs.Shadow("#000000", 5, 5, 10);
+            this.addChild(this._menuTitleLabel);
             // 5x5 Box Blur filter on bg image
             var blurFilter = new createjs.BlurFilter(5, 5);
             this._backgroundImage.filters = [blurFilter];
             var bitmapBounds = this._backgroundImage.getBounds();
             this._backgroundImage.cache(bitmapBounds.x, bitmapBounds.y, bitmapBounds.width, bitmapBounds.height);
             this._playBtn = new objects.Button("PlayBtn", config.Screen.CENTER_X, config.Screen.CENTER_Y + 150);
+            this._playBtn.shadow = new createjs.Shadow("#000", 5, 5, 10);
             this.addChild(this._playBtn);
             this._playBtn.on("click", this._playBtnClick, this);
             // Add menu scene to global stage container
--- Scripts/scenes/menu.ts
+++ Scripts/scenes/menu.ts
@@ -9,6 +9,7 @@ module scenes {
         // Private instance variables
         private _backgroundImage: createjs.Bitmap;
         // Label or bitmap
+        private _menuTitleLabel: objects.Label
         // Button 
         private _playBtn: objects.Button;
         // Menu Class Contructor
@@ -23,6 +24,17 @@ module scenes {
             this._backgroundImage = new createjs.Bitmap(assets.getResult("Bg"))
             this.addChild(this._backgroundImage)
 
+            // title Label
+            this._menuTitleLabel = new objects.Label(
+                "Cops and Robbers",
+                "90px Comic Sans MS",
+                "#f7e907",
+                config.Screen.CENTER_X,
+                config.Screen.CENTER_Y - 100
+            )
+            this._menuTitleLabel.shadow = new createjs.Shadow("#000000", 5, 5, 10);
+            this.addChild(this._menuTitleLabel)
+
             // 5x5 Box Blur filter on bg image
             let blurFilter = new createjs.BlurFilter(5, 5);
             this._backgroundImage.filters = [blurFilter];
@@ -31,6 +43,7 @@ module scenes {
             this._backgroundImage.cache(bitmapBounds.x, bitmapBounds.y, bitmapBounds.width, bitmapBounds.height);
 
             this._playBtn = new objects.Button("PlayBtn", config.Screen.CENTER_X, config.Screen.CENTER_Y + 150);
+            this._playBtn.shadow = new createjs.Shadow("#000", 5, 5, 10)
             this.addChild(this._playBtn);
             this._playBtn.on("click", this._playBtnClick, this);
 
