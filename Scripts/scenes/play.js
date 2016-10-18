c526a78d7ddc581d2284c5fe852cddf31b79f79f
Kevin Ma <kbma.kevin@gmail.com>
2016-10-18 09:58:45 -0400

    added score and time label to play scene

--- Scripts/scenes/play.js
+++ Scripts/scenes/play.js
@@ -13,6 +13,12 @@ var scenes;
         Play.prototype.start = function () {
             this._backgroundImage = new createjs.Bitmap(assets.getResult("Bg"));
             this.addChild(this._backgroundImage);
+            this._scoreLabel = new objects.Label("Score: 0", "40px comic sans ms", "#f7e907", 90, 40);
+            this._scoreLabel.shadow = new createjs.Shadow("#000", 5, 5, 15);
+            this.addChild(this._scoreLabel);
+            this._timeLabel = new objects.Label("Time: 0", "40px comic sans ms", "#f7e907", 100, 90);
+            this._timeLabel.shadow = new createjs.Shadow("#000", 5, 5, 15);
+            this.addChild(this._timeLabel);
             this._enemy = new objects.Enemy("robber", 2);
             this.addChild(this._enemy);
             stage.addChild(this);
